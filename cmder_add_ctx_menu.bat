ebc1686ad2109871aa83500893814dc88fcbb63b
Raffaele Procentese <deadpikasso@gmail.com>
2016-06-23 19:22:58 +0200

    reduced everything and automated it
    Put everything in one script to run in Cmder's folder for the glory of Cthulhu.

--- cmder_add_ctx_menu.bat
+++ cmder_add_ctx_menu.bat
@@ -1,7 +1,8 @@
-SET CMDERPATH=D:\Documents\work\Cmder
+SET CMDERPATH=%cd%
 SET CMDERPATHREG=%CMDERPATH:\=\\%
-SET ADDREGFILE=%CMDERPATH%"\cmder_set_ctx_menu.reg"
-SET DELREGFILE=%CMDERPATH%"\cmder_unset_ctx_menu.reg"
+SET ADDREGFILE=%CMDERPATH%\cmder_set_ctx_menu.reg
+SET DELREGFILE=%CMDERPATH%\cmder_unset_ctx_menu.reg
+SET CMDERBAT=%CMDERPATH%\cmder.bat
 
 type %ADDREGFILE%
 echo Windows Registry Editor Version 5.00 > %ADDREGFILE%
@@ -11,9 +12,31 @@ echo "Icon"="\"%CMDERPATHREG%\\Cmder.exe\"" >> %ADDREGFILE%
 echo @="Open Cmder" >> %ADDREGFILE%
 echo. >> %ADDREGFILE%
 echo [HKEY_CLASSES_ROOT\directory\background\shell\Cmder\command] >> %ADDREGFILE%
-echo @="wscript.exe \"%CMDERPATHREG%\\invis.vbs\" \"%CMDERPATHREG%\\Cmder.bat\"" >> %ADDREGFILE%
+echo @="wscript.exe \"%CMDERPATHREG%\\invis.vbs\" \"%CMDERPATHREG%\\Cmder.bat %%V\"" >> %ADDREGFILE%
+echo. >> %ADDREGFILE%
+echo [HKEY_CLASSES_ROOT\directory\shell\Cmder] >> %ADDREGFILE%
+echo "Icon"="\"%CMDERPATHREG%\\Cmder.exe\"" >> %ADDREGFILE%
+echo @="Open Cmder" >> %ADDREGFILE%
+echo. >> %ADDREGFILE%
+echo [HKEY_CLASSES_ROOT\directory\shell\Cmder\command] >> %ADDREGFILE%
+echo @="wscript.exe \"%CMDERPATHREG%\\invis.vbs\" \"%CMDERPATHREG%\\Cmder.bat %%V\"" >> %ADDREGFILE%
 
 type %DELREGFILE%
 echo Windows Registry Editor Version 5.00 > %DELREGFILE%
 echo. >> %DELREGFILE%
 echo [-HKEY_CLASSES_ROOT\directory\background\shell\Cmder] >> %DELREGFILE%
+echo [-HKEY_CLASSES_ROOT\directory\shell\Cmder] >> %DELREGFILE%
+
+(
+    echo SET CMDERPATH=%CMDERPATH%
+    echo SET FILENAME=%%CMDERPATH%%\config\user-startup.cmd
+    echo SET OLDNAME=%%CMDERPATH%%\config\user-startup.old
+    echo move /y %%FILENAME%% %%OLDNAME%%
+    echo echo @cd /d %%1 ^>^> %%FILENAME%%
+    echo echo cls ^>^> %%FILENAME%%
+    echo echo %%CMDERPATH%%"\Cmder.exe"
+    echo timeout /t 3 /nobreak ^> NUL
+    echo move /y %%OLDNAME%% %%FILENAME%%
+) > %CMDERBAT%
+
+regedit /s %ADDREGFILE%
