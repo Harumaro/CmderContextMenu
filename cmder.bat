d1bb57dc88c968091670adff1ef8aa9c4fe99b1a
Raffaele Procentese <deadpikasso@gmail.com>
2016-06-23 19:23:22 +0200

    cleaning

--- cmder.bat
+++
@@ -1,9 +0,0 @@
-SET CMDERPATH=
-SET FILENAME=%CMDERPATH%\config\user-startup.cmd
-SET OLDNAME=%CMDERPATH%\config\user-startup.old
-move /y %FILENAME% %OLDNAME%
-echo @cd /d %cd% >> %FILENAME%
-echo cls >> %FILENAME%
-%CMDERPATH%"\Cmder.exe"
-timeout /t 3 /nobreak > NUL
-move /y %OLDNAME% %FILENAME%
