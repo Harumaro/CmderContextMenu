0e5cf4b1c324890c59d731c9d50c8767d8dd7ff9
Raffaele Procentese <deadpikasso@gmail.com>
2016-06-23 18:34:30 +0200

    preserve old user configuration

--- cmder.bat
+++ cmder.bat
@@ -1,5 +1,9 @@
-SET CMDERPATH=D:\Documents\work\Cmder
-SET FILENAME=%CMDERPATH%"\config\user-startup.cmd"
+SET CMDERPATH=
+SET FILENAME=%CMDERPATH%\config\user-startup.cmd
+SET OLDNAME=%CMDERPATH%\config\user-startup.old
+move /y %FILENAME% %OLDNAME%
 echo @cd /d %cd% >> %FILENAME%
 echo cls >> %FILENAME%
 %CMDERPATH%"\Cmder.exe"
+timeout /t 3 /nobreak > NUL
+move /y %OLDNAME% %FILENAME%
