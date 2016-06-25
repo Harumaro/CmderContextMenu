4af78c012499d01658b7c62adefa6e692efb95ba
Raffaele Procentese <deadpikasso@gmail.com>
2016-06-25 11:59:19 +0200

    it's $100% ready now
    - script installs everything and creates a removal tool then deletes itself
    - removal tool deletes everything then deletes itself
    
    either run it from within cmder folder or provide a directory as argument

--- cmder_add_ctx_menu.bat
+++ cmder_add_ctx_menu.bat
@@ -1,49 +1,70 @@
-IF "%1" == "" SET CMDERPATH=%cd%
-ELSE SET CMDERPATH=%1
+@echo off
+
+IF "%1" == "" (SET CMDERPATH=%cd%) ELSE (SET CMDERPATH=%1)
 
 SET CMDERPATHREG=%CMDERPATH:\=\\%
 SET INVISFILE=%CMDERPATH%\invis.vbs
-SET ADDREGFILE=%CMDERPATH%\cmder_set_ctx_menu.reg
-SET DELREGFILE=%CMDERPATH%\cmder_unset_ctx_menu.reg
+SET ADDREGFILE=%~dp0cmder_set_ctx_menu.reg
 SET CMDERBAT=%CMDERPATH%\Cmder.bat
+SET CMDERCTXUNINS=%~dp0cmder_remove_ctx_menu.bat
 
-type %ADDREGFILE%
-echo Windows Registry Editor Version 5.00 > %ADDREGFILE%
-echo. >> %ADDREGFILE%
-echo [HKEY_CLASSES_ROOT\directory\background\shell\Cmder] >> %ADDREGFILE%
-echo "Icon"="\"%CMDERPATHREG%\\Cmder.exe\"" >> %ADDREGFILE%
-echo @="Open Cmder" >> %ADDREGFILE%
-echo. >> %ADDREGFILE%
-echo [HKEY_CLASSES_ROOT\directory\background\shell\Cmder\command] >> %ADDREGFILE%
-echo @="wscript.exe \"%CMDERPATHREG%\\invis.vbs\" \"%CMDERPATHREG%\\Cmder.bat %%V\"" >> %ADDREGFILE%
-echo. >> %ADDREGFILE%
-echo [HKEY_CLASSES_ROOT\directory\shell\Cmder] >> %ADDREGFILE%
-echo "Icon"="\"%CMDERPATHREG%\\Cmder.exe\"" >> %ADDREGFILE%
-echo @="Open Cmder" >> %ADDREGFILE%
-echo. >> %ADDREGFILE%
-echo [HKEY_CLASSES_ROOT\directory\shell\Cmder\command] >> %ADDREGFILE%
-echo @="wscript.exe \"%CMDERPATHREG%\\invis.vbs\" \"%CMDERPATHREG%\\Cmder.bat %%V\"" >> %ADDREGFILE%
+(
+    echo Windows Registry Editor Version 5.00 
+    echo.
+    echo [HKEY_CLASSES_ROOT\directory\background\shell\Cmder]
+    echo "Icon"="\"%CMDERPATHREG%\\Cmder.exe\""
+    echo @="Open Cmder"
+    echo.
+    echo [HKEY_CLASSES_ROOT\directory\background\shell\Cmder\command]
+    echo @="wscript.exe \"%CMDERPATHREG%\\invis.vbs\" \"%CMDERPATHREG%\\Cmder.bat %%V\""
+    echo.
+    echo [HKEY_CLASSES_ROOT\directory\shell\Cmder]
+    echo "Icon"="\"%CMDERPATHREG%\\Cmder.exe\""
+    echo @="Open Cmder"
+    echo.
+    echo [HKEY_CLASSES_ROOT\directory\shell\Cmder\command]
+    echo @="wscript.exe \"%CMDERPATHREG%\\invis.vbs\" \"%CMDERPATHREG%\\Cmder.bat %%V\""
+) > %ADDREGFILE%
 
-type %DELREGFILE%
-echo Windows Registry Editor Version 5.00 > %DELREGFILE%
-echo. >> %DELREGFILE%
-echo [-HKEY_CLASSES_ROOT\directory\background\shell\Cmder] >> %DELREGFILE%
-echo [-HKEY_CLASSES_ROOT\directory\shell\Cmder] >> %DELREGFILE%
-
-type %INVISFILE%
 echo CreateObject("Wscript.Shell")^.Run "" ^& WScript.Arguments(0) ^& "", 0, False > %INVISFILE%
 
 (
-    echo SET CMDERPATH=%CMDERPATH%
-    echo SET FILENAME=%%CMDERPATH%%\config\user-startup.cmd
-    echo SET OLDNAME=%%CMDERPATH%%\config\user-startup.old
+    echo ^@echo off
+    echo.
+    echo set CMDERPATH=%CMDERPATH%
+    echo set FILENAME=%%CMDERPATH%%\config\user-startup.cmd
+    echo set OLDNAME=%%CMDERPATH%%\config\user-startup.old
+    echo.
     echo move /y %%FILENAME%% %%OLDNAME%%
+    echo.
     echo echo @cd /d %%1 ^>^> %%FILENAME%%
     echo echo cls ^>^> %%FILENAME%%
     echo %%CMDERPATH%%\Cmder.exe
+    echo.
     echo timeout /t 3 /nobreak ^> NUL
     echo move /y %%OLDNAME%% %%FILENAME%%
 ) > %CMDERBAT%
 
+(
+    echo ^@echo off
+    echo SET DELREGFILE=%%~dp0cmder_unset_ctx_menu.reg
+    echo set INVISFILE=%INVISFILE%
+    echo set CMDERBAT=%CMDERBAT%
+    echo.
+    echo ^(
+    echo echo Windows Registry Editor Version 5.00
+    echo echo.
+    echo echo [-HKEY_CLASSES_ROOT\directory\background\shell\Cmder]
+    echo echo [-HKEY_CLASSES_ROOT\directory\shell\Cmder]
+    echo ^) ^> %%DELREGFILE%%
+    echo.
+    echo regedit /s %%DELREGFILE%%
+    echo del /f %%DELREGFILE%%
+    echo del /f %%INVISFILE%%
+    echo del /f %%CMDERBAT%%
+    echo del %%~f0
+) > %CMDERCTXUNINS%
+
 regedit /s %ADDREGFILE%
 del /f %ADDREGFILE%
+del %~f0
